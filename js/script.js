f8f9689def82abb3e4e85f483df65c51595e96b7
vghybrid <anderson.sarmiento13@gmail.com>
2022-02-11 17:39:37 -0300

    cambios

--- js/script.js
+++ js/script.js
@@ -1,14 +1,4 @@
-let menuBoton = document.querySelector(".menu-boton");
-let botonCerrar = document.querySelector(".boton-cerrar");
-let navBar = document.querySelector(".navbar");
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
 
-menuBoton.onclick = function() {
-    menuBoton.style.opacity = "0";
-    menuBoton.style.pointerEvents = "none";
-    navBar.classList.add("active");
-}
-botonCerrar.onclick = function() {
-    menuBoton.style.opacity = "1";
-    menuBoton.style.pointerEvents = "auto";
-    navBar.classList.remove("active");
-}
