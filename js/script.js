74fa3d44d10f5ec96d73a2ed7a39684985cba749
vghybrid <anderson.sarmiento13@gmail.com>
2022-02-11 20:28:21 -0300

    cambio final

--- js/script.js
+++ js/script.js
@@ -2,3 +2,16 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show');
 });
 
+const form = async () => {
+    try {
+        const result = await axios.post('https://sistemacaliva.com/api/front-test', {
+            headers: {
+                'test-key': 'eyJhbGciOiJIUzI1NiJ9.eyJSb2xlIjoiZGV2ZWxvcGVyIiwiSXNzdWVyI-joiSXNzdWVyIiwiVXNlcm5hbWUiOiJGcm9udERldiIsImV4cCI6MTYyODY5NTg3NiwiaWF0IjoxNjI4Njk1ODc2fQ.CDRPz6Eta78BzmuNTNZsnzzDU2TRgvEtMs-_aZlWCZQ'
+            }
+        })
+        console.log(result);
+    } catch (error) {
+        console.log(error);
+    }
+
+}
